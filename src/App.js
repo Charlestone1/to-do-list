7a73355a8f486eeba2a0566c00d0ec58db45b7fa
Charles Oriih <charlesorih007@gmail.com>
2023-02-12 18:12:37 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [name, setName] = useState("");
   const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
-  const [isCompleted, setIsCompleted] = useState(false);
+  // const [isCompleted, setIsCompleted] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
     show: false,
